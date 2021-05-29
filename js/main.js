21be70dca4289d2f9dab8a4aa9a29ae1923ec84a
Juan Ignacio Marín <ignacio.bendwise@aleeas.com>
2021-05-28 23:01:15 -0300

    adding diferent colors to buttons

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 // html formating meibi?
 
+const colors = ["80ff72","e58c8a","7ee8fa","0a369d","7a5c58","007c77","89023e","1c77c3","b80c09","6184d8"];
+
 function addAudioId() {
 
     const audioClips = [...document.getElementsByClassName("clip")];
     const buttons = [...document.getElementsByClassName("drum-pad")];
 
     for (let i = 0; i < audioClips.length; i++) {
+
+        //set button colors
+        buttons[i].style.backgroundColor = "#" + colors[i];
+
+        // set the id
         audioClips[i].id = buttons[i].innerText;
 
         //add innet text to audioclips
