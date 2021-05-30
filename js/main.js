eba5a759859fe05b0d92bd6af8e568fe60a05e8a
Juan Ignacio Marín <ignacio.bendwise@aleeas.com>
2021-05-30 14:49:19 -0300

    updating styling and some functions

--- js/main.js
+++ js/main.js
@@ -18,31 +18,6 @@ function showVolValue(value) {
     changeVolume(value / 100);
 }
 
-
-// html formating meibi?
-
-const colors = ["80ff72","e58c8a","7ee8fa","0a369d","7a5c58","007c77","89023e","1c77c3","b80c09","6184d8"];
-
-function addAudioId() {
-
-    const audioClips = [...document.getElementsByClassName("clip")];
-    const buttons = [...document.getElementsByClassName("drum-pad")];
-
-    for (let i = 0; i < audioClips.length; i++) {
-
-        //set button colors
-        buttons[i].style.backgroundColor = "#" + colors[i];
-
-        // set the id
-        audioClips[i].id = buttons[i].innerText;
-
-        //add innet text to audioclips
-        audioClips[i].innerText = buttons[i].id;
-    }
-
-    changeVolume(0.4);
-}
-
 // display the note you are playing
 
 function displayNote(audio) {
@@ -55,10 +30,8 @@ function displayNote(audio) {
 // play audio 
 
 let pauseThis = "";
-function playAudio(id) {
-    const button = document.getElementById(id).innerText;
-    
-    let audio = document.getElementById(button);
+function playAudio(innerText) {
+    let audio = document.getElementById(innerText);
 
     if (pauseThis) {
         pauseThis.pause();
@@ -76,20 +49,37 @@ function playAudio(id) {
 
 document.addEventListener('keypress', playOnKeypress);
 
+
 function playOnKeypress(event) {
     let key = event.keyCode;
+    event.preventDefault();
 
     let char = String.fromCharCode(key).toUpperCase();
 
-    let audio = document.getElementById(char);
+    const audio = document.getElementById(char);
+    const button = document.getElementById(audio.innerText);
 
-    if (pauseThis) {
-        pauseThis.pause();
-        pauseThis.currentTime = 0;
+    button.click();
+}
+
+
+// html formating meibi?
+
+function addAudioId() {
+
+    const audioClips = [...document.getElementsByClassName("clip")];
+    const buttons = [...document.getElementsByClassName("drum-pad")];
+
+    for (let i = 0; i < audioClips.length; i++) {
+
+        // set the id
+        audioClips[i].id = buttons[i].innerText;
+
+        //add inner text to audioclips
+        audioClips[i].innerText = buttons[i].id;
+
     }
 
-    pauseThis = audio;
-
-    audio.play();
-    displayNote(audio);
+    changeVolume(0.4);
 }
+
