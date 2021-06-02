1775e860e02d00b96c083245f1378bd0d56528f5
Juan Ignacio Marín <ignacio.bendwise@aleeas.com>
2021-06-01 23:02:46 -0300

    update everthing

--- js/main.js
+++ js/main.js
@@ -1,85 +1,41 @@
-// change volume
+// Variables
+
+const volume = document.querySelector(".volume")
+const display = document.getElementById("display")
+const keypad = document.querySelector(".keypad")
+
+const audioClips = [...keypad.querySelectorAll(".clip")]
+
+let sound = ""
+
+
+// Volumen changing
 
 function changeVolume(number) {
-    const audioClips = [...document.getElementsByClassName("clip")];
-
     audioClips.forEach(element => {
-        element.volume = number;
+        element.volume = number / 100
     });
 }
 
-// the range input and the volume
+// Listening to clicks and keypresses
 
-function showVolValue(value) {
-    let volume = document.getElementById("volume");
-
-    volume.innerText = value;
-
-    changeVolume(value / 100);
-}
-
-// display the note you are playing
-
-function displayNote(audio) {
-    const display = document.getElementById("display");
-
-    display.innerText = audio.innerText;
-}
-
-
-// play audio 
-
-let pauseThis = "";
-function playAudio(innerText) {
-    let audio = document.getElementById(innerText);
-
-    if (pauseThis) {
-        pauseThis.pause();
-        pauseThis.currentTime = 0;
+const playAudio = (audio) => {
+    if (sound) {
+    sound.pause()
+    sound.currentTime = 0
     }
 
-    pauseThis = audio;
+    sound = audio
 
-    audio.play();
-    displayNote(audio);
+    audio.play()
+    display.textContent = audio.textContent
 }
 
-
-// listen to keypresses
-
-document.addEventListener('keypress', playOnKeypress);
-
-
-function playOnKeypress(event) {
-    let key = event.keyCode;
-    event.preventDefault();
-
-    let char = String.fromCharCode(key).toUpperCase();
-
-    const audio = document.getElementById(char);
-    const button = document.getElementById(audio.innerText);
-
-    button.click();
-}
-
-
-// html formating meibi?
-
-function addAudioId() {
-
-    const audioClips = [...document.getElementsByClassName("clip")];
-    const buttons = [...document.getElementsByClassName("drum-pad")];
-
-    for (let i = 0; i < audioClips.length; i++) {
-
-        // set the id
-        audioClips[i].id = buttons[i].innerText;
-
-        //add inner text to audioclips
-        audioClips[i].innerText = buttons[i].id;
-
-    }
-
-    changeVolume(0.4);
-}
+document.addEventListener('keypress', event => {
+    let key = event.keyCode
+    let char = String.fromCharCode(key).toUpperCase()
+    
+    let button = document.getElementById(char).parentElement
+    button.click()
+})
 
